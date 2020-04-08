5dbf3a676ec0da1087e9f906cf4246789dffb378
haidar <haidattkj26@gmail.com>
2020-04-08 17:00:15 +0700

    view for flip to start

---
+++ AppleDev-MC1/FlipToStartVC.swift
@@ -0,0 +1,30 @@
+//
+//  FlipToStartVC.swift
+//  AppleDev-MC1
+//
+//  Created by Muhammad Haidar Rais on 08/04/20.
+//  Copyright © 2020 Rahmat Zulfikri. All rights reserved.
+//
+
+import UIKit
+
+class FlipToStartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
