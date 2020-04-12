223c0c4e2e2e199343975be5fc2d2df1e463d47f
Steven Tandianus <standianus@gmail.com>
2020-04-12 21:54:38 +0700

    Update

---
+++ AppleDev-MC1/Models/History 2.swift
@@ -0,0 +1,16 @@
+//
+//  History.swift
+//  AppleDev-MC1
+//
+//  Created by Rahmat Zulfikri on 12/04/20.
+//  Copyright © 2020 Rahmat Zulfikri. All rights reserved.
+//
+
+import Foundation
+
+struct History {
+    var ambienceId: Int?
+    var activityName: String?
+    var duration: Int?
+    var isComplete: Bool = false
+}
