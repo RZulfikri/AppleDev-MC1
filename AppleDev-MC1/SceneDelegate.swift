8da33c54a9fe76bb7383ac48cf7360bc1e8d750d
Nathanael Evan <nathanaelyehudaevan@gmail.com>
2020-04-12 20:11:32 +0700

    Update Gambar Musik
    Update 5 gambar dan musik

--- AppleDev-MC1/SceneDelegate.swift
+++ AppleDev-MC1/SceneDelegate.swift
@@ -14,15 +14,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            window = UIWindow()
-            window?.windowScene = windowScene
-            let layout = UICollectionViewFlowLayout()
-            layout.scrollDirection = .horizontal
-            let swipingController = OnboardSwipingController(collectionViewLayout: layout)
-            
-            window?.rootViewController = swipingController
-            window?.makeKeyAndVisible()
+//            guard let windowScene = (scene as? UIWindowScene) else { return }
+//            window = UIWindow()
+//            window?.windowScene = windowScene
+//            let layout = UICollectionViewFlowLayout()
+//            layout.scrollDirection = .horizontal
+//            let swipingController = OnboardSwipingController(collectionViewLayout: layout)
+//            
+//            window?.rootViewController = swipingController
+//            window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
